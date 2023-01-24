385b40554fa743be5a4674e6036deabff7ae8336
Ilham1606 <16ilhamapriansyah@gmail.com>
2023-01-24 19:23:10 +0700

    Tugas pertemuan ke-2

---
+++ tugas1.py
@@ -0,0 +1,11 @@
+for i in range(1, 51):
+    if i % 3 == 0 and i % 5 == 0:
+        print("DangDut")
+    elif i % 3 == 0:
+        print("Dang")
+    elif i % 5 == 0:
+        print("Dut")
+    else:
+        print(i)
+
+print("Tugas pertemuan ke-2 ~ Ilham Apriansyah")
